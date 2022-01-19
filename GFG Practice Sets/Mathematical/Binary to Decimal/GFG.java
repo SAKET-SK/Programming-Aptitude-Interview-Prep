70b9c0fceded9fb4e53e296d59daa8becf90f5ff
Saket Khopkar <saketkhopkar478@gmail.com>
2022-01-19 19:43:00 +0530

    Create GFG.java

---
+++ GFG Practice Sets/Mathematical/Binary to Decimal/GFG.java
@@ -0,0 +1,39 @@
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();            
+            Solution ob = new Solution();
+            int ans = ob.binary_to_decimal(str);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int binary_to_decimal(String str)
+    {
+        // Code here
+        int m=0;
+        int k=1;
+        for(int i=str.length()-1;i>=0;i--){
+            m+=(str.charAt(i)-'0')*k;
+            k=k*2;
+        }
+        return m;
+    }
+}
