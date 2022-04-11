458da3de8bdebce89fff62ae2228f59ce2c74829
Saket Khopkar <saketkhopkar478@gmail.com>
2022-04-11 23:23:41 +0530

    Create Solution.cpp

---
+++ GFG Practice Sets/Greedy Algorithms/Fractional Knapsack/Solution.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+ // } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        double ans = 0.0;
+        priority_queue<pair<double, int>> pq;
+        for(int i = 0;i<n;i++)
+        {
+            pq.push({(double)arr[i].value/(double)arr[i].weight, arr[i].weight});
+        }
+        while(!pq.empty())
+        {
+            if(W==0) return ans;
+            int diff = min(pq.top().second, W);
+            ans += diff*pq.top().first;
+            W -= diff;
+            pq.pop();
+        }
+        return ans;
+    }
+        
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(2)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
